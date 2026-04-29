05ecdda63faf0a611f43ba464a7bcac45d3a9ca8
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2026-04-29 13:46:19 -0400

    style: :speech_balloon: improve formatting in html

--- solution.py
+++ solution.py
@@ -947,30 +947,31 @@ apply_and_show_random_image(simple_net, dataset)
 # <h4>Question: Receptive Field Size</h4>
 # What are the receptive field sizes of the following operations?
 #
-# 1. <code style="color: black">torch.nn.Conv2d(1, 5, 3)</code>
-# 2. <code style="color: black">torch.nn.Sequential(torch.nn.Conv2d(1, 5, 3), torch.nn.Conv2d(5,5,3))</code>
-# 3. <code style="color: black">torch.nn.Sequential(torch.nn.Conv2d(1, 5, 3), torch.nn.Conv2d(5,5,5))</code>
-# 4. <code style="color: black">Downsample(3)</code>
-# 5. <code style="color: black">torch.nn.Sequential(ConvBlock(1, 5, 3), Downsample(2), ConvBlock(5,5,3)</code>
-# 6. <code style="color: black">torch.nn.Upsample(2)</code>
-# 7. <code style="color: black">torch.nn.Sequential(ConvBlock(1,5,3), Upsample(2), ConvBlock(5,5,3))</code>
-# 8. <code style="color: black">torch.nn.Sequential(ConvBlock(1,5,3), Downsample(3), ConvBlock(5,5,3), Upsample(3), ConvBlock(5,5,3))</code>
-# 9. <code style="color: black">UNet(depth=2, in_channels=1, downsample_factor=3, kernel_size=3)</code>
-#
+# <ol>
+# <li> <code style="color: black">torch.nn.Conv2d(1, 5, 3)</code></li>
+# <li> <code style="color: black">torch.nn.Sequential(torch.nn.Conv2d(1, 5, 3), torch.nn.Conv2d(5,5,3))</code></li>
+# <li> <code style="color: black">torch.nn.Sequential(torch.nn.Conv2d(1, 5, 3), torch.nn.Conv2d(5,5,5))</code></li>
+# <li> <code style="color: black">Downsample(3)</code></li>
+# <li> <code style="color: black">torch.nn.Sequential(ConvBlock(1, 5, 3), Downsample(2), ConvBlock(5,5,3)</code></li>
+# <li> <code style="color: black">torch.nn.Upsample(2)</code></li>
+# <li> <code style="color: black">torch.nn.Sequential(ConvBlock(1,5,3), Upsample(2), ConvBlock(5,5,3))</code></li>
+# <li> <code style="color: black">torch.nn.Sequential(ConvBlock(1,5,3), Downsample(3), ConvBlock(5,5,3), Upsample(3), ConvBlock(5,5,3))</code></li>
+# <li> <code style="color: black">UNet(depth=2, in_channels=1, downsample_factor=3, kernel_size=3)</code></li>
 #
 # </div>
 
 # %% [markdown] tags=["solution"]
 # <div class="alert alert-block alert-warning">
-# 1. 3x3
-# 2. 5x5
-# 3. 7x7
-# 4. 3x3
-# 5. 14x14
-# 6. 1x1
-# 7. 8x8
-# 8. 22x22
-# 9. 22x22
+# <ol>
+# <li> 3x3 </li>
+# <li> 5x5 </li>
+# <li> 7x7 </li>
+# <li> 3x3 </li>
+# <li> 14x14 </li>
+# <li> 1x1 </li>
+# <li> 8x8 </li>
+# <li> 22x22 </li>
+# <li> 22x22 </li>
 # </div>
 
 # %% [markdown] tags=[]
